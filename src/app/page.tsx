9f1c6d9a2594e9f3b4e67cf8ca935a4f0d4870f3
vargaae <vargaae@hotmail.com>
2024-03-29 00:50:25 +0100

    01003-setUp Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main className="flex text-textPrimary">SportDATA MAIN</main>;
+  const nd = new Date();
+  const dateConvert = nd.toDateString();
+
+  return (
+    <section className="px-2 md:px-4 md:w-[600px]">
+      <div className="flex justify-between items-center mb-4 md:mb-2">
+        <h1 className="text-md md:text-xl font-bold">MATCHES</h1>
+        <div className="px-4 py-0 md:py-1 bg-slate-600 rounded-md text-textSecondary text-sm">
+          <p>{`${dateConvert}`}</p>
+        </div>
+      </div>
+      {/* data matches */}
+    </section>
+  );
 }
