d5d7b60a7b0a9220fb67ba8a75476578ac7d5cd2
vargaae <vargaae@hotmail.com>
2024-03-30 13:51:58 +0100

    02007-Create Competition component

---
+++ src/components/competition/Competition.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+import { matchesType } from "@/types";
+
+const Competition = ({ matches }: { matches: matchesType }) => {
+  const imageLoader = ({}) => {
+    return `${matches?.competition?.emblem}`;
+  };
+  const nd = new Date(matches?.utcDate);
+  const dateConvert = nd.toLocaleDateString() + " " + nd.toLocaleTimeString();
+
+  return (
+    <div className="bg-slate-600 hover:bg-slate-700 rounded-md mb-2 mb-4 flex justify-between items-center px-4 py-1">
+      <div className="flex space-x-4">
+        <Image
+          loader={imageLoader}
+          src={matches?.competition?.emblem}
+          alt={matches?.competition.name}
+          width={20}
+          height={20}
+        />
+        <p className="text-sm text-teal-400">{matches?.competition.name}</p>
+      </div>
+      <p className="text-xs md:text-sm">{dateConvert}</p>
+    </div>
+  );
+};
+
+export default Competition;
