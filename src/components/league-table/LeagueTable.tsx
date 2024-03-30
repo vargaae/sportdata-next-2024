e4a5404abbec9ad79698a93639aeee09d1c1c7f3
vargaae <vargaae@hotmail.com>
2024-03-30 14:11:43 +0100

    02008-Create Matches component

--- src/components/league-table/LeagueTable.tsx
+++ src/components/league-table/LeagueTable.tsx
@@ -1,10 +1,12 @@
 import { matchesType } from "@/types";
 import Competition from "../competition/Competition";
+import Matches from "../matches/Matches";
 
 const LeagueTable = ({ matches }: { matches: matchesType }) => {
   return (
     <div className="py-3 px-2 md:px-3 rounded-md flex flex-col bg-[rgb(45,59,87)] text-primary mb-2">
       <Competition matches={matches} />
+      <Matches matches={matches} />
       {/* <Matches matches={matches} /> */}
     </div>
   );
---
+++ src/components/matches/Matches.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { matchesType } from "@/types";
+
+const Matches = ({ matches }: { matches: matchesType }) => {
+  const imageLoaderHome = `${matches?.homeTeam?.crest!}`;
+  // Refactor it to one function: const src = `${API}/user/photo/${blog.postedBy.username}`; <Image loader={() => src} src={src} width={20} height={20}/>
+  const imageLoaderAway = ({}) => {
+    return `${matches?.awayTeam?.crest!}`;
+  };
+  const getDate = new Date(matches?.utcDate).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return (
+    <div className="grid grid-cols-3">
+      <div className="w-full flex items-center">
+        <div className="w-[20px] h-[20px] relative mr-2">
+          <Image
+            unoptimized
+            loader={() => imageLoaderHome}
+            src={imageLoaderHome}
+            alt={matches?.homeTeam?.name!}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <p className="text-sm">{matches?.homeTeam?.name}</p>
+      </div>
+      <div className="px-2 m-auto flex justify-center items-center bg-slate-600 rounded-md">
+        {matches?.status == "FINISHED" ? (
+          <p className="py-1 text-teal-400 text-xs">
+            {matches?.score?.fullTime.home} : {matches.score?.fullTime.away}
+          </p>
+        ) : (
+          <p className="py-1 text-teal-400 text-xs">{getDate}</p>
+        )}
+      </div>
+      <div className="w-full flex items-center justify-end">
+        <p className="text-sm text-right">{matches.awayTeam?.name}</p>
+        <div className="w-[20px] h-[20px] relative ml-2">
+          <Image
+            unoptimized
+            loader={imageLoaderAway}
+            src={matches?.awayTeam?.crest!}
+            alt={matches.awayTeam?.name!}
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Matches;
