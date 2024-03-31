d187ca5136ed4d93e09de45c49dd4c2813a346a7
vargaae <vargaae@hotmail.com>
2024-04-01 00:56:30 +0200

    04003-unify styling of header-MATCHES-LEAGUES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   return (
     <section className="px-2 md:px-4 md:w-[600px]">
       <div className="flex justify-between items-center mb-4 md:mb-2">
-        <h1 className="text-md md:text-xl font-bold">MATCHES</h1>
+      <h1 className="px-4 py-0 md:py-1 bg-teal-100 rounded-md text-md md:text-xl font-bold">MATCHES</h1>
         <div className="px-4 py-0 md:py-1 bg-slate-600 rounded-md text-textSecondary text-sm">
           <p>{`${dateConvert}`}</p>
         </div>
