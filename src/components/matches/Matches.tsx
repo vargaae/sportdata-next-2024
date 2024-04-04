1909bbfa9641966d48588c61766257f9dd597a14
vargaae <vargaae@hotmail.com>
2024-04-04 02:49:44 +0200

    0404-HotFix Matches-Finished

--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -9,39 +9,54 @@ const Matches = ({ matches }: { matches: matchesType }) => {
 
   return (
     <div className="grid grid-cols-3">
-      <div className="w-full flex items-center">
-        <div className="w-[20px] h-[20px] relative mr-2">
-          <Image
-            unoptimized
-            src={matches?.homeTeam?.crest!}
-            alt={matches?.homeTeam?.name!}
-            fill
-            className="object-cover"
-          />
-        </div>
-        <p className="text-sm">{matches?.homeTeam?.name}</p>
-      </div>
-      <div className="px-2 m-auto flex justify-center items-center bg-slate-600 rounded-md">
-        {matches?.status == "FINISHED" ? (
-          <p className="py-1 text-teal-400 text-xs">
-            {matches?.score?.fullTime?.home} : {matches?.score?.fullTime?.away}
-          </p>
-        ) : (
-          <p className="py-1 text-teal-400 text-xs">Match start: {getDate}</p>
-        )}
-      </div>
-      <div className="w-full flex items-center justify-end">
-        <p className="text-sm text-right">{matches.awayTeam?.name}</p>
-        <div className="w-[20px] h-[20px] relative ml-2">
-          <Image
-            unoptimized
-            src={matches?.awayTeam?.crest!}
-            alt={matches?.awayTeam?.name!}
-            fill
-            className="object-cover"
-          />
-        </div>
-      </div>
+      {matches?.status == "FINISHED" ? (
+        <>
+          <div className="w-full flex items-center">
+            <div className="w-[20px] h-[20px] relative mr-2"></div>
+            <p className="text-sm">{matches?.homeTeam?.name}</p>
+          </div>
+          <div className="px-2 m-auto flex justify-center items-center bg-slate-600 rounded-md">
+            <p className="py-1 text-teal-400 text-xs">
+              {matches?.score?.fullTime?.home} :{" "}
+              {matches?.score?.fullTime?.away}
+            </p>
+          </div>
+          <div className="w-full flex items-center justify-end">
+            <p className="text-sm text-right">{matches.awayTeam?.name}</p>
+            <div className="w-[20px] h-[20px] relative ml-2"></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="w-full flex items-center">
+            <div className="w-[20px] h-[20px] relative mr-2">
+              <Image
+                unoptimized
+                src={matches?.homeTeam?.crest!}
+                alt={matches?.homeTeam?.name!}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <p className="text-sm">{matches?.homeTeam?.name}</p>
+          </div>
+          <div className="px-2 m-auto flex justify-center items-center bg-slate-600 rounded-md">
+            <p className="py-1 text-teal-400 text-xs">Match start: {getDate}</p>
+          </div>
+          <div className="w-full flex items-center justify-end">
+            <p className="text-sm text-right">{matches.awayTeam?.name}</p>
+            <div className="w-[20px] h-[20px] relative ml-2">
+              <Image
+                unoptimized
+                src={matches?.awayTeam?.crest!}
+                alt={matches?.awayTeam?.name!}
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/status/Status.tsx
+++ src/components/status/Status.tsx
@@ -51,7 +51,7 @@ const Status = ({
         {statusMatch === "FINISHED"
           ? matchesList?.toReversed().map((matches) => (
               <div key={matches.id}>
-                {matches.status === "TIMED" && (
+                {matches.status === "FINISHED" && (
                   <LeagueTable matches={matches} />
                 )}
               </div>
@@ -60,7 +60,7 @@ const Status = ({
         {statusMatch === "FINISHED"
           ? matchesListFinished?.toReversed().map((matches) => (
               <div key={matches.id}>
-                {matches.status === "TIMED" && (
+                {matches.status === "FINISHED" && (
                   <LeagueTable matches={matches} />
                 )}
               </div>
